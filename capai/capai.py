e0157e848a16489a467216e06571bb6ec6aceb5e
krist <36255856+krishna9626@users.noreply.github.com>
2020-06-22 17:46:26 +0530

    Update capai.py

--- capai/capai.py
+++ capai/capai.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import argparse
 import os
-
 import getopt
 from .database import Database
 from .langConfig import LangConfig
@@ -9,12 +8,12 @@ from .parser import Parser
 from .stopwordFilter import StopwordFilter
 from .thesaurus import Thesaurus
 from .constants import Color, without_color
+from .settings import DEBUG
 
-class CapAI:
+class capai:
     def __init__(
             self,
             database_path,
-            input_sentence,
             language_path,
             json_output_path=None,
             thesaurus_path=None,
@@ -24,9 +23,9 @@ class CapAI:
         if color == False:
             without_color()
 
-        database = Database()
+        database = Database(database_path)
         self.stopwordsFilter = None
-
+        language_path = os.path.dirname(os.path.abspath(__file__)) + '/lang_store/english.csv'
         if thesaurus_path:
             thesaurus = Thesaurus()
             thesaurus.load(thesaurus_path)
@@ -36,9 +35,8 @@ class CapAI:
             self.stopwordsFilter = StopwordFilter()
             self.stopwordsFilter.load(stopwords_path)
 
-        database.load(database_path)
-        # database.print_me()
-
+        database.load()
+        #database.print_me()
         config = LangConfig()
         config.load(language_path)
 
@@ -66,33 +64,34 @@ class CapAI:
             os.remove(filename)
 
 def print_help_message():
-    if settings.DEBUG :
+    if DEBUG :
         print ('\n')
         print ('Usage:')
-        print ('\tpython ln2sql.py -d <path> -l <path> -i <input-sentence> [-t <path>] [-j <path>]')
+        print ('\tpython capai.py -d <path> -l <path> -i <input-sentence> [-t <path>] [-j <path>]')
         print ('Parameters:')
         print ('\t-h\t\t\tprint this help message')
-        print ('\t-d <path>\t\tpath to SQL dump file')
+        print ('\t-d <path>\t\tpath to SQL schema')
         print ('\t-l <path>\t\tpath to language configuration file')
         print ('\t-i <input-sentence>\tinput sentence to parse')
         print ('\t-j <path>\t\tpath to JSON output file')
         print ('\t-t <path>\t\tpath to thesaurus file')
         print ('\n')
 
+
 def main(argv):
     # try:
     opts, args = getopt.getopt(argv,"d:l:i:t:j:")
     database_path = None
     input_sentence = None
-    language_path = None
+    language_path = os.path.dirname(os.path.abspath(__file__)) + '/lang_store/english.csv',
     thesaurus_path = None
     json_output_path = None
 
     for i in range(0, len(opts)):
         if opts[i][0] == "-d":
             database_path = opts[i][1]
-        elif opts[i][0] == "-l":
-            language_path = opts[i][1]
+        #elif opts[i][0] == "-l":
+            #language_path = opts[i][1]
         elif opts[i][0] == "-i":
             input_sentence = opts[i][1]
         elif opts[i][0] == "-j":
@@ -102,10 +101,10 @@ def main(argv):
         else:
             print_help_message()
             # sys.exit()
-            raise getopt.GetoptError('ln2sqlmodule : Invalid args received',None)
+            raise getopt.GetoptError('capai : Invalid args received',None)
     
     if (database_path is None) or (input_sentence is None) or (language_path is None):
-        raise getopt.GetoptError('ln2sqlmodule : Invalid args received',None)
+        raise getopt.GetoptError('capai : Invalid args received',None)
     else:
         if thesaurus_path is not None:
             thesaurus_path = str(thesaurus_path)
@@ -113,6 +112,15 @@ def main(argv):
             json_output_path = str(json_output_path)
 
     #try:
-    ln2sqlObj = ln2sql(str(database_path), str(input_sentence), str(language_path), thesaurus_path, json_output_path)
+    ln2sqlObj = capai(str(database_path), str(language_path), thesaurus_path, json_output_path) 
     
     return ln2sqlObj.get_query(str(input_sentence))
+    #except Exception, e:
+    #    print color.BOLD + color.RED + str(e) + color.END
+
+    # except getopt.GetoptError:
+    #     print_help_message()
+
+
+# if __name__ == '__main__':
+#     main(sys.argv[1:])
